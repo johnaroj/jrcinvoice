ed4338f589672958da8694695915a317d2e14779
john rojas <johnaroj@hotmail.com>
2025-01-04 09:16:32 +0100

    feat: background dark view

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -31,7 +31,7 @@ export default function OnboardingPage() {
 
   return (
     <div className="min-h-screen w-screen flex items-center justify-center">
-      <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
+      <div className="absolute inset-0 -z-10 h-full w-full bg-background bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
         <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)]"></div>
       </div>
       <Card className="max-w-sm mx-auto">
